751b1ced5624ead61398df019200f30dd4c338ca
Akira Komamura <akira.komamura@gmail.com>
2025-01-05 14:22:11 +0900

    feat!: Provide a new lib API that is consistent with twist.nix

--- flake.nix
+++ flake.nix
@@ -3,11 +3,7 @@
     twist.url = "github:emacs-twist/twist.nix";
   };
 
-  outputs =
-    { ... }@inputs:
-    {
-      overlays.default = _: super: {
-        emacsTwist2Elpa = import ./nix inputs super;
-      };
-    };
+  outputs = inputs: {
+    lib = import ./nix inputs;
+  };
 }
--- nix/default.nix
+++ nix/default.nix
@@ -1,15 +1,13 @@
 let
   inherit (builtins) mapAttrs attrValues;
 in
-inputs@{ twist, ... }:
-pkgs@{ lib, ... }:
+{ twist, ... }:
 let
   inherit (twist.lib) buildElispPackage;
 
-  makeSnapshotVersion = import ./makeSnapshotVersion.nix { inherit lib; };
-
   # Convert a value from packageInputs for the ELPA context.
   convertAttrs =
+    { lib }:
     attrs:
     attrs
     // {
@@ -17,26 +15,41 @@ let
       dontByteCompile = true;
       wantExtraOutputs = true;
       nativeCompileAhead = false;
-      version = makeSnapshotVersion {
+      version = import ./makeSnapshotVersion.nix { inherit lib; } {
         sourceInfo = attrs.sourceInfo or null;
       } (attrs.version or null);
     };
 
-  convertToElpaArchive = pkgs.callPackage ./convertToElpaArchive.nix { };
+  convertToElpaArchive = { pkgs }: pkgs.callPackage ./convertToElpaArchive.nix { };
 
-  buildElpaPackage = attrs: convertToElpaArchive attrs (buildElispPackage pkgs attrs);
+  makeElpaArchiveContents = import ./makeElpaArchiveContents.nix;
+in
+rec {
+  # Build a single package that can be installed using `package-install-file`.
+  buildElpaPackage =
+    { pkgs }:
+    attrs:
+    let
+      elpaAttrs = convertAttrs { inherit (pkgs) lib; } attrs;
+    in
+    convertToElpaArchive { inherit pkgs; } elpaAttrs (buildElispPackage pkgs elpaAttrs);
 
-  makeElpaArchiveContents = import ./makeElpaArchiveContents.nix { inherit lib; };
+  # Build an entire package archive that can be specified in `package-archives`
+  # custom variable. `buildElpaArchive` builds a directory that can be served
+  # from a server like S3, and `buildElpaArchiveAsTar` builds a tar archive that
+  # can be distributed from GitHub Releases (Note: it's recommended to compress
+  # the tar archive before you upload it).
 
   buildElpaArchive =
-    opts@{
+    {
+      pkgs,
       withInstaller ? false,
     }:
     packageInputs:
     let
-      packageInputs' = mapAttrs (_: convertAttrs) packageInputs;
-      packageEntries = mapAttrs (_: buildElpaPackage) packageInputs';
-      tarCommands = lib.mapAttrsToList (name: attrs: ''
+      packageInputs' = mapAttrs (_: convertAttrs { inherit (pkgs) lib; }) packageInputs;
+      packageEntries = mapAttrs (_: buildElpaPackage { inherit pkgs; }) packageInputs';
+      tarCommands = pkgs.lib.mapAttrsToList (name: attrs: ''
         ( name="${attrs.ename}-${attrs.version}" \
         && tar --mode u+w -cf "$out/$name.tar" \
            --transform "s,^,$name/," \
@@ -44,15 +57,15 @@ let
            .
         )
       '') packageInputs';
-      installerScript = import ./makeInstaller.nix { inherit lib; } packageInputs';
+      installerScript = import ./makeInstaller.nix { inherit (pkgs) lib; } packageInputs';
     in
     pkgs.runCommand "elpa-archive"
       {
         preferLocalBuild = true;
         allowSubstitutes = false;
         passthru.entries = attrValues packageEntries;
-        archiveContents = makeElpaArchiveContents packageInputs';
-        installerScript = lib.optionalString withInstaller installerScript;
+        archiveContents = makeElpaArchiveContents { inherit (pkgs) lib; } packageInputs';
+        installerScript = pkgs.lib.optionalString withInstaller installerScript;
         passAsFile = [
           "archiveContents"
           "installerScript"
@@ -60,7 +73,7 @@ let
       }
       ''
         mkdir -p $out
-        ${lib.concatStrings tarCommands}
+        ${pkgs.lib.concatStrings tarCommands}
         if [[ -s "$installerScriptPath" ]]
         then
           cat "$installerScriptPath" > $out/install-all.el
@@ -69,7 +82,8 @@ let
       '';
 
   buildElpaArchiveAsTar =
-    opts: name: packageInputs:
+    { pkgs, ... }@opts:
+    name: packageInputs:
     pkgs.runCommand "elpa-archive"
       {
         preferLocalBuild = true;
@@ -81,15 +95,4 @@ let
         name="${name}"
         tar --mode u+w -cf "$out/$name.tar" --transform "s,^,$name/," -C $root .
       '';
-in
-{
-  # Build a single package that can be installed using `package-install-file`.
-  buildElpaPackage = attrs: buildElpaPackage (convertAttrs attrs);
-
-  # Build an entire package archive that can be specified in `package-archives`
-  # custom variable. `buildElpaArchive` builds a directory that can be served
-  # from a server like S3, and `buildElpaArchiveAsTar` builds a tar archive that
-  # can be distributed from GitHub Releases (Note: it's recommended to compress
-  # the tar archive before you upload it).
-  inherit buildElpaArchive buildElpaArchiveAsTar;
 }
